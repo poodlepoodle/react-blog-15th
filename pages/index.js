148f1de651176fa9f38324e4c3e1a60982887620
poodlepoodle <chammal97@naver.com>
2022-05-18 21:41:39 +0900

    :lipstick: [style]: use style-jsx in index.js
    use style-jsx in index.js selectively

--- pages/index.js
+++ pages/index.js
@@ -1,91 +1,34 @@
 import Head from 'next/head';
 import Image from 'next/image';
+
+import icon_home from '../public/icons/home.png';
+import icon_write from '../public/icons/write.png';
+
 import styles from '../styles/Home.module.css';
 
-import styled from 'styled-components';
-
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-`;
-
-const TopNavigator = styled.div`
-  display: flex;
-
-  height: 100px;
-
-  background-color: red;
-`;
-
-const TopNavigatorIconBox = styled.div`
-  display: flex;
-
-  background-color: purple;
-`;
-
-const TopNavigatorIcon = styled.img`
-  width: 30px;
-  height: auto;
-`;
-
-const TopNavigatorTitle = styled.div`
-  font-size: 32px;
-`;
-
-const TopNavigatorSearchForm = styled.form`
-  background-color: green;
-`;
-
-const TopNavigatorSearchInput = styled.input`
-  background-color: yellow;
-`;
-
-const PageContainer = styled.div`
-  flex: 1;
-
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  background-color: blue;
-`;
-
-const MainPostContainer = styled.div`
-  display: flex;
-`;
-
-const MainPostItem = styled.div``;
-
-const MainPostItemThumbnailWrapper = styled.div``;
-
-const MainPostItemThumbnail = styled.img`
-  width: 300px;
-  height: auto;
-`;
-
-export default function Home() {
+const Home = () => {
   return (
     // <div className={styles.container}>
-    <Wrapper>
+    <div className={styles.wrapper}>
       <Head>
         <title>Poodle Problem.log</title>
         <meta name="description" content="~.~ Poodle Problem.log ~.~" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <TopNavigator>
-        <TopNavigatorIconBox>
-          <TopNavigatorIcon />
-          <TopNavigatorIcon />
-        </TopNavigatorIconBox>
-        <TopNavigatorTitle>Poodle Problem.log</TopNavigatorTitle>
-        <TopNavigatorSearchForm>
-          <TopNavigatorSearchInput />
-        </TopNavigatorSearchForm>
-      </TopNavigator>
+      <div className="nav">
+        <div className="nav-icon-box">
+          <Image width={20} src={icon_home} />
+          <Image width={20} src={icon_write} />
+        </div>
+        <div className="nav-title">Poodle Problem.log</div>
+        <div className="nav-search">
+          <input />
+        </div>
+      </div>
 
       {/* <main className={styles.main}> */}
-      <PageContainer>main page</PageContainer>
+      <div>main page</div>
 
       {/* <footer className={styles.footer}>
         <a
@@ -99,6 +42,8 @@ export default function Home() {
           </span>
         </a>
       </footer> */}
-    </Wrapper>
+    </div>
   );
-}
+};
+
+export default Home;
