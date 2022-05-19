b244b14e444e8899fda8768dc594eb719cdf6feb
probloom-solbin-de <101960628+probloom-solbin-de@users.noreply.github.com>
2022-05-20 03:09:59 +0900

    :construction: [feat] Test data fetching
    Test brunch for data fetching

--- component/CardItem.js
+++ component/CardItem.js
@@ -1,32 +1,21 @@
 import React from 'react';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 import styles from '../styles/BlogMain.module.css';
 import bearEmoji from '../public/emojis/bear-face.png';
 
-const CardItem = ({ posts }) => {
+const CardItem = ({ title, info, text }) => {
   return (
     <div>
-      {posts.map((post) => (
-        <div className={styles.CardContainer}>
-          <Image className={styles.ItemImage} src={bearEmoji} />
-          <div className={styles.ItemInfoWrapper}>
-            <div className={styles.ItemMainTextWrapper}>
-              <div className={styles.ItemTitle}>가짜타이틀{post.title}</div>
-              <div className={styles.ItemInfo}>가짜인포{post.info}</div>
-            </div>
-            <div className={styles.ItemText}>
-              가짜텍스트가 아주 길게 있어요 가짜텍스트가 아주 길게
-              있어요가짜텍스트가 아주 길게 있어요가짜텍스트가 아주 길게
-              있어요가짜텍스트가 아주 길게 있어요 가짜텍스트가 아주 길게
-              있어요가짜텍스트가 아주 길게 있어요가짜텍스트가 아주 길게 있어요
-              가짜텍스트가 아주 길게 있어요가짜텍스트가 아주 길게
-              있어요가짜텍스트가 아주 길게 있어요가짜텍스트가 아주 길게 있어요
-              {post.text}
-            </div>
+      <div className={styles.CardContainer}>
+        <Image className={styles.ItemImage} src={bearEmoji} />
+        <div className={styles.ItemInfoWrapper}>
+          <div className={styles.ItemMainTextWrapper}>
+            <div className={styles.ItemTitle}>{title}</div>
+            <div className={styles.ItemInfo}>{info}</div>
           </div>
+          <div className={styles.ItemText}>{text}</div>
         </div>
-      ))}
+      </div>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,37 +1,32 @@
-import Head from 'next/head';
-import Image from 'next/image';
-
-import icon_home from '../public/icons/home.png';
-import icon_write from '../public/icons/write.png';
+import CardItem from '../component/CardItem';
 
 import styles from '../styles/Home.module.css';
 
-const Home = () => {
+/*
+{posts.map((post) => ( 
+  ))}
+        title={post.title}
+        info={post.dateCreated}
+        text={post.bodyText}
+
+  */
+
+const Home = ({ posts }) => {
   return (
     // <div className={styles.container}>
-    <div className={styles.wrapper}>
-      {/* <Head>
-        <title>Poodle Problem.log</title>
-        <meta name="description" content="~.~ Poodle Problem.log ~.~" />
-        <link rel="icon" href="/favicon.ico" /> */}
-      {/* </Head> */}
-
-      <div>main page</div>
-
-      {/* <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer> */}
+    /* <main className={styles.main}> */
+    <div>
+      <CardItem title={'타이틀'} info={'인포'} text={'텍스트'}></CardItem>
     </div>
   );
 };
+
+export async function getServerSideProps(context) {
+  //const res = await fetch(`https://posts`);
+  //const { posts } = await res.json();
+  return {
+    props: {},
+  };
+}
 
 export default Home;
