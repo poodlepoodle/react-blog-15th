3d5303624886856c7224a2fd17085af142702ccc
poodlepoodle <chammal97@naver.com>
2022-05-21 19:46:10 +0900

    :bug: try to fix a bug
    try hard to fix a bug

--- pages/index.js
+++ pages/index.js
@@ -15,21 +15,22 @@ export const getStaticProps = async () => {
 };
 
 const Home = ({ postsList }) => {
+  console.log(postsList);
+
   return (
     <div className={styles.wrapper}>
       {postsList.map((post) => (
-        <Link href="/posts/[id]" as={`/posts/${post.id}`}>
-          <a>
-            <CardItem
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              info={post.dateCreated}
-              text={post.bodyText}
-              {...postsList}
-            />
-          </a>
-        </Link>
+        // <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+        //   <a>
+        <CardItem
+          key={post.id}
+          title={post.title}
+          info={post.dateCreated}
+          text={post.bodyText}
+          {...postsList}
+        />
+        //   </a>
+        // </Link>
       ))}
     </div>
   );
