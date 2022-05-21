64b31bc6bc84331b4a4448162f36f65466246db7
poodlepoodle <chammal97@naver.com>
2022-05-21 17:51:05 +0900

    :bug: [chore]: fix a bug
    try hard to fix a deploy bug

--- pages/index.js
+++ pages/index.js
@@ -18,18 +18,18 @@ const Home = ({ postsList }) => {
   return (
     <div className={styles.wrapper}>
       {postsList.map((post) => (
-        <Link href="/posts/[id]" as={`/posts/${post.id}`}>
-          <a>
-            <CardItem
-              key={post.id}
-              id={post.id}
-              title={post.title}
-              info={post.dateCreated}
-              text={post.bodyText}
-              {...postsList}
-            />
-          </a>
-        </Link>
+        // <Link href="/posts/[id]" as={`/posts/${post.id}`}>
+        //   <a>
+        <CardItem
+          key={post.id}
+          id={post.id}
+          title={post.title}
+          info={post.dateCreated}
+          text={post.bodyText}
+          {...postsList}
+        />
+        //   </a>
+        // </Link>
       ))}
     </div>
   );
