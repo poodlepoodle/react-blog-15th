3c746660249e096a9113ca5dba59946f1bb37c27
poodlepoodle <chammal97@naver.com>
2022-05-22 00:37:17 +0900

    :lipstick: [style]: update new post editor ui
    update new post editor ui

--- pages/components/WriteField.js
+++ pages/components/WriteField.js
@@ -1,28 +1,44 @@
 import React from 'react';
 import Image from 'next/image';
-import styles from '../../styles/PostItem.module.css';
+
+import styles from '../../styles/WriteField.module.css';
+
 import icon_delete from '../../public/icons/delete.png';
 import icon_write from '../../public/icons/write.png';
 
 const WriteField = () => {
   return (
-    <div className={styles.PostWrapper}>
-      <div className={styles.PostContainer}>
-        <div className={styles.ItemInfoWrapper}>
-          <div className={styles.ItemMainTextWrapper}>
-            <input className={styles.ItemTitle} />
-            <div className={styles.ItemInfo}>인포</div>
+    <div className={styles.wrapper}>
+      <div className={styles.container}>
+        <div className={styles.info_wrapper}>
+          <div className={styles.info_maintext_wrapper}>
+            <input
+              className={styles.info_title_input}
+              placeholder="포스트 제목을 입력하세요"
+            />
+            {/* <div className={styles.item_info}>인포</div> */}
+            {/* <input
+              type="date"
+              id="post-date"
+              name="post-date"
+              value="2018-07-22"
+              min="2022-01-01"
+              max="2022-12-31"
+            /> */}
           </div>
-          <div className={styles.navbar_right_wrapper}>
-            <div className={styles.navbar_icon_wrapper}>
+          <div className={styles.item_btn_wrapper}>
+            <div className={styles.item_btn_icon_wrapper}>
               <Image src={icon_delete} />
             </div>
-            <div className={styles.navbar_icon_wrapper}>
+            <div className={styles.item_btn_icon_wrapper}>
               <Image src={icon_write} />
             </div>
           </div>
         </div>
-        <input className={styles.ItemText} />
+        <textarea
+          className={styles.item_textarea}
+          placeholder="포스트 내용을 입력하세요"
+        />
       </div>
     </div>
   );
